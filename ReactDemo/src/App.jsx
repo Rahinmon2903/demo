83a06cdb5c1e030fcd06c4ec7d7bd0f2a7780ad6
Rahinmon2903 <rahin2903@gmail.com>
2025-10-22 20:54:38 +0530

    baic of react

---
+++ ReactDemo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import About from './components/about';
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Footer/>
+            <About/>
+
+           
+        </div>
+    );
+};
+
+export default App;
