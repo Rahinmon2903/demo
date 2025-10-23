052690a35ffb52f3f8306b6fb5749b5235a95d51
Rahinmon2903 <rahin2903@gmail.com>
2025-10-23 11:26:51 +0530

    react practice

---
+++ Reactpractice1/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Header() {
+    return (
+        <div>
+            <h1 className='text-[27px]'>header</h1>
+
+        </div>
+    );
+}
+
+export default Header;
