9f5d01da9f85282725a50ca1ce999b043d30e491
Rahinmon2903 <rahin2903@gmail.com>
2025-10-23 12:30:31 +0530

    i commented the tailwind page now  I am going to practice pasiing data from parent to child

--- Reactpractice1/src/App.jsx
+++ Reactpractice1/src/App.jsx
@@ -6,9 +6,9 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
-      <Header/>
+      {/* <Header/>
       <Home/>
-      <Footer/>
+      <Footer/> */}
       
     </div>
   );
