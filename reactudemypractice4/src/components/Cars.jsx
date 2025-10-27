ba9a4974a77fff65fe1da06ec45ba46e490b0429
Rahinmon2903 <rahin2903@gmail.com>
2025-10-27 10:35:10 +0530

    little update on css

--- reactudemypractice4/src/components/Cars.jsx
+++ reactudemypractice4/src/components/Cars.jsx
@@ -5,12 +5,12 @@ const Cars = (props) => {
     <>
       <div className="card">
         <div className="top">
-          <h2>{props.name}</h2>
-          <img src={props.img} alt="avatar_img" />
+          <h2 className="name">{props.name}</h2>
+          <img  className="circle-img"src={props.img} alt="avatar_img" />
         </div>
         <div className="bottom">
-          <p>{props.phone}</p>
-          <p>{props.email}</p>
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
     </>
