56fc454935c26d2319c9df6eed02056aae645afc
Rahinmon2903 <rahin2903@gmail.com>
2025-10-28 12:48:37 +0530

    amazon cart logic

--- ReactHooks/src/App.jsx
+++ ReactHooks/src/App.jsx
@@ -1,20 +1,26 @@
 import React, {useState} from 'react';
+import CompA from './Component/CompA';
 
 const App = () => {
-  const [value,setValue]=useState(10);
+  const [value,setValue]=useState(0);
+  const [status,useStatus]=useState(true);
   const add =() =>{
+    useStatus(false);
     setValue(value+1);
 
   }
   const sub=()=>{
+     useStatus(true);
     setValue(value==0?0:value-1);
 
   }
   return (
     <div>
       <h1>{value}</h1>
-      <button onClick={add}>Add</button>
-      <button onClick={sub}>Sub</button>
+      {status ?  <button onClick={add}>Add to cart </button> : <button onClick={sub}>remove to cart</button> }
+     
+      
+      <CompA status={status} useStatus={useStatus}/>
       
     </div>
   );
---
+++ ReactHooks/src/Component/CompA.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CompA = ({useStatus}) => {
+
+      const add =() =>{
+        useStatus(false);
+
+
+  }
+  const sub=()=>{
+    
+    
+
+  }
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CompA;
